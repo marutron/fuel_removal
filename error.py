2f87ef48a6150cb0cfef7bd2ced7f0415f3601e4
marat <ahmatov-marat8@mail.ru>
2026-03-19 13:50:51 +0400

    add tvs counting

--- error.py
+++ error.py
@@ -4,3 +4,12 @@ class CustomFileNotFound(FileNotFoundError):
 
     def __str__(self):
         return f"\nНе найден файл: {self.file_path}.\nВосстановите его и перезапустите скрипт."
+
+
+class AzExportException(Exception):
+
+    def __init__(self, tvs_number: str, tvs_coord: str):
+        self.tvs_coord = tvs_coord
+
+    def __str__(self):
+        return f"\nПопытка вывезти ТВС не из БВ (координаты: {self.tvs_coord})"
